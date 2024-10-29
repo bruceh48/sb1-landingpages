a57cfe6b6891cb8eaadedc7c42a7b55c20421cbe
bruceh48 <brucehearder@gmail.com>
2024-10-29 15:21:47 +0800

    Initial commit

---
+++ src/data/cities.ts
@@ -0,0 +1,14 @@
+export const cityData = [
+  {
+    city: 'Kalgoorlie',
+    reason: "Kalgoorlie is a significant mining centre, leading to a constant demand for electrical services in both residential and commercial sectors, particularly in mining operations and maintenance. There is a shortage of skilled electricians in the area, providing an opportunity for new businesses to fill this gap and cater to local needs. Ongoing infrastructure projects and housing developments create additional demand for electrical installations and repairs."
+  },
+  {
+    city: 'Busselton',
+    reason: "As a popular tourist destination, Busselton experiences seasonal spikes in demand for electrical services, particularly in hospitality and residential sectors. The city is undergoing residential expansion, requiring electrical work for new homes and renovations. Busselton's strong community focus can lead to repeat business and referrals, essential for building a local clientele."
+  },
+  {
+    city: 'Geraldton',
+    reason: "Regional Development: Geraldton is seeing growth in various sectors, including agriculture and renewable energy, which increases the need for electrical services. The mix of residential, commercial, and industrial clients provides multiple avenues for business development. Its position as a regional hub allows easy access to surrounding areas, expanding the potential market for electrical services."
+  }
+];
